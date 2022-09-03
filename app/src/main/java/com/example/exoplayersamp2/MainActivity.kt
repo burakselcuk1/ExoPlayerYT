bd14e9f9658b48de54302d4aa9fd9cd1bd8cdad7
burak <design.burakk@gmail.com>
2022-09-04 00:16:10 +0300

    first commit by burak baba

---
+++ app/src/main/java/com/example/exoplayersamp2/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.exoplayersamp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.exoplayersamp2.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        preparePlayer()
+    }
+
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        binding.playerView.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem = MediaItem.fromUri(URL)
+        val mediaSource =
+            DashMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.seekTo(playbackPosition)
+        exoPlayer?.playWhenReady = playWhenReady
+        exoPlayer?.prepare()
+    }
+
+    private fun  relasePlayer(){
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        relasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        relasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        relasePlayer()
+    }
+
+    companion object{
+        const val URL =
+            "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd"
+    }
+}
